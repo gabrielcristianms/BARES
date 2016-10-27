17acc2d27d9c5e8304542a29e7b0158323ce0cab
Selan R. dos Santos <selan.rds@gmail.com>
2016-10-27 14:50:01 -0300

    Fixing header in token.h

--- token.h
+++ token.h
@@ -1,21 +1,24 @@
 #ifndef _TOKEN_H_
 #define _TOKEN_H_
 
+#include <string>
+
 /// Estrutura que representa um token, composto por seu valor (string) e seu tipo (enum).
 struct Token
 {
     public:
         enum token_t
         {
-            OPERAND = 0, // Basicamente números
-            OPERATOR,    // "+", "-", "*", "/", "%", "^" 
+            OPERAND = 0, // Basicamente números.
+            OPERATOR,    // "+", "-", "*", "/", "%", "^".
             SCOPE        // "(" ou ")"
         };
 
         std::string value; //<! Valor do token.
         token_t type;      //<! Tipo de token: operando, operador, escopo.
 
-        Token( std::string v_="", token_t t_ = OPERAND )
+        /// Construtor default.
+        explicit Token( std::string v_="", token_t t_ = OPERAND )
             : value( v_ )
             , type( t_ )
         {/* empty */}

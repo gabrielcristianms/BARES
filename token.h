6ed05fe98b9c9923cee0e6990c8dd50e46c4381c
Selan R. dos Santos <selan.rds@gmail.com>
2016-10-27 14:40:21 -0300

    Add token struct to represent a Token.

---
+++ token.h
@@ -0,0 +1,24 @@
+#ifndef _TOKEN_H_
+#define _TOKEN_H_
+
+/// Estrutura que representa um token, composto por seu valor (string) e seu tipo (enum).
+struct Token
+{
+    public:
+        enum token_t
+        {
+            OPERAND = 0, // Basicamente números
+            OPERATOR,    // "+", "-", "*", "/", "%", "^" 
+            SCOPE        // "(" ou ")"
+        };
+
+        std::string value; //<! Valor do token.
+        token_t type;      //<! Tipo de token: operando, operador, escopo.
+
+        Token( std::string v_="", token_t t_ = OPERAND )
+            : value( v_ )
+            , type( t_ )
+        {/* empty */}
+};
+
+#endif

f85976241c8953754e7445b5956f5e127be095b3
Gabriel Cristian <gabrielcristianms@gmail.com>
2016-11-12 08:20:56 -0300

    This commit finishes the signature of the evaluator class

--- evaluator.h
+++ evaluator.h
@@ -1,8 +1,15 @@
 #ifndef _EVALUATOR_H_
 #define _EVALUATOR_H_
 
+#include <stack>     // stack
+#include <string>    // string
+#include <cassert>   // assert
+#include <cmath>     // pow
+
 class Evaluator{
 	public:
+		typedef short int result_t;
+
 		struct EvaluatorResult{
 			typedef size_t size_type;
 
@@ -22,14 +29,47 @@ class Evaluator{
 		};
 
 		EvaluatorResult evaluate( std::vector<Token> e_ );
-		//TODO resto da classe evaluate
+		result_t get_result() const ;
 
 		/// Constutor default.
-        Parser() = default;
-        ~Parser() = default;
+        Evaluator() = default;
+        ~Evaluator() = default;
         /// Desligar cópia e atribuição.
-        Parser( const Parser & ) = delete;  // Construtor cópia.
-        Parser & operator=( const Parser & ) = delete; // Atribuição.
-};
+        Evaluator( const Evaluator & ) = delete;  // Construtor cópia.
+        Evaluator & operator=( const Evaluator & ) = delete; // Atribuição.
 
+    private:
+    	std::string<Token> infix_expr;
+    	std::stack<Token> posfix_expr;
+
+    	/// Converts a expression in infix notation to a corresponding profix representation.
+		std::stack<Token> infix_to_postfix( std::string );
+
+		/// Checks whether the first operator has higher precedence over the second one.
+		bool has_higher_precedence(Token , Token);
+
+		/// Checks if the token works by right association.
+		bool right_association( Token );
+
+		/// Checks whether a token is operator symbol or not. 
+		bool is_operator(Token);
+
+		/// Checks whether a token is a character is alphanumeric chanaracter (letter or numeric digit) or not. 
+		bool is_operand(Token);
+
+		/// Checks whether the token is an opening scope symbol
+		bool is_opening_scope(Token);
+
+		/// Checks whether the token is a closing scope symbol
+		bool is_closing_scope(Token);
+
+		/// Returns the precedence of the operator.
+		result_t get_operator_precedence(Token);
+
+		/// Return the value of a token.
+		result_t char_2_int( Token );
+
+		/// This is where we calculate values and return them.
+		result_t apply_operation( result_t op1, result_t op2, Token ch );
+};
 #endif
